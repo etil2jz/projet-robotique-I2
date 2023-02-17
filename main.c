33ba3a08fe1f021196336dca32c62bb06bb01aa0
etil.sol <81570777+etil2jz@users.noreply.github.com>
2023-02-17 11:51:15 +0100

    feature: control servo with IR

--- main.c
+++ main.c
@@ -1,12 +1,16 @@
 #include <Wire.h>
 #include <VL6180X.h>
+#include <Servo.h>
 
 VL6180X sensor;
 int distance;
+Servo servo;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  servo.attach(A1);
+  
   Wire.begin();
   sensor.init();
   sensor.configureDefault();
@@ -17,6 +21,11 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   distance = sensor.readRangeSingle();
+  int position = map(distance, 10, 200, 0, 180);
+  servo.write(position);
+  int speed = map(distance, 10, 200, 10, 100);
+  servo.writeMicroseconds(1500);
+  servo.writeMicroseconds(1500 + speed);
   Serial.println(distance);
   delay(100);
 }
