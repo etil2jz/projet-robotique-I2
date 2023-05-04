775740d05cb92c92daec984d9ab8efeed17d7d1a
etil.sol <81570777+etil2jz@users.noreply.github.com>
2023-05-04 09:29:51 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,16 +2,195 @@
 #include <VL6180X.h>
 #include <Servo.h>
 
-VL6180X sensor1, sensor2, sensor3; // Déclaration des 3 capteurs
-Servo servo1, servo2; // Déclaration des 2 servomoteurs
-unsigned char distance1, distance2, distance3; // Déclaration des 3 variables de distance
+VL6180X capteurG, capteurF, capteurD; // Déclaration des 3 capteurs
+Servo servoG, servoD; // Déclaration des 2 servomoteurs
+unsigned char distanceG, distanceF, distanceD; // Déclaration des 3 variables de distance
 
 void setup() {
   // Initialiser la communication Serial et I2C
-  Serial.begin(9600);
-  while (!Serial);
+  //Serial.begin(9600);
+  //while (!Serial);
   Wire.begin();
   Wire.setClock(400000); // 400 kHz
+
+  /*pinMode(8, OUTPUT);
+
+tone(8,294,125);//D4
+delay(125);
+tone(8,294,125);//D4
+delay(125);
+tone(8,587,250);//D5
+delay(250);
+tone(8,440,250);//A4
+delay(375);
+tone(8,415,125);//Ab4
+delay(250);
+tone(8,392,250);//G4
+delay(250);
+tone(8,349,250);//F4
+delay(250);
+tone(8,294,125);//D4
+delay(125);
+tone(8,349,125);//F4
+delay(125);
+tone(8,392,125);//G4
+delay(125);
+tone(8,261,125);//C4(middle)     
+delay(62);
+tone(8,261,125);//C4(middle)     
+delay(62);
+tone(8,261,125);//C4(middle)     
+delay(62);
+tone(8,261,125);//C4(middle)     
+delay(62);
+tone(8,587,250);//D5
+delay(250);
+tone(8,440,375);//A4
+delay(375);
+tone(8,415,125);//Ab4
+delay(250);
+tone(8,392,250);//G4
+delay(250);
+tone(8,349,250);//F4
+delay(250);
+tone(8,294,125);//D4
+delay(125);
+tone(8,349,125);//F4
+delay(125);
+tone(8,392,125);//G4
+delay(125);
+tone(8,247,125);//B3
+delay(125);
+tone(8,247,125);//B3
+delay(125);
+tone(8,587,250);//D5
+delay(250);
+tone(8,440,375);//A4
+delay(375);
+tone(8,415,125);//Ab4
+delay(250);
+tone(8,392,250);//G4
+delay(250);
+tone(8,349,250);//F4
+delay(250);
+tone(8,294,125);//D4
+delay(125);
+tone(8,349,125);//F4
+delay(125);
+tone(8,392,125);//G4
+delay(125);
+tone(8,233,62);//Bb3
+delay(62);
+tone(8,233,62);//Bb3
+delay(62);
+tone(8,233,62);//Bb3
+delay(62);
+tone(8,233,62);//Bb3
+delay(62);
+tone(8,587,250);//D5
+delay(250);
+tone(8,440,375);//A4
+delay(375);
+tone(8,415,125);//Ab4
+delay(250);
+tone(8,392,250);//G4
+delay(250);
+tone(8,349,250);//F4
+delay(250);
+tone(8,294,125);//D4
+delay(125);
+tone(8,349,125);//F4
+delay(125);
+tone(8,392,125);//G4
+delay(125);
+tone(8,294,125);//D4
+delay(125);
+tone(8,294,125);//D4
+delay(125);
+tone(8,587,250);//D5
+delay(250);
+tone(8,440,375);//A4
+delay(375);
+tone(8,415,125);//Ab4
+delay(250);
+tone(8,392,250);//G4
+delay(250);
+tone(8,349,250);//F4
+delay(250);
+tone(8,294,125);//D4
+delay(125);
+tone(8,349,125);//F4
+delay(125);
+tone(8,392,125);//G4
+delay(125);
+tone(8,261,125);//C4(middle)     
+delay(62);
+tone(8,261,125);//C4(middle)     
+delay(62);
+tone(8,261,125);//C4(middle)     
+delay(62);
+tone(8,261,125);//C4(middle)     
+delay(62);
+tone(8,587,250);//D5
+delay(250);
+tone(8,440,375);//A4
+delay(375);
+tone(8,415,125);//Ab4
+delay(250);
+tone(8,392,250);//G4
+delay(250);
+tone(8,349,250);//F4
+delay(250);
+tone(8,294,125);//D4
+delay(125);
+tone(8,349,125);//F4
+delay(125);
+tone(8,392,125);//G4
+delay(125);
+tone(8,247,125);//B3
+delay(125);
+tone(8,247,125);//B3
+delay(125);
+tone(8,587,250);//D5
+delay(250);
+tone(8,440,375);//A4
+delay(375);
+tone(8,415,125);//Ab4
+delay(250);
+tone(8,392,250);//G4
+delay(250);
+tone(8,349,250);//F4
+delay(250);
+tone(8,294,125);//D4
+delay(125);
+tone(8,349,125);//F4
+delay(125);
+tone(8,392,125);//G4
+delay(125);
+tone(8,233,62);//Bb3
+delay(62);
+tone(8,233,62);//Bb3
+delay(62);
+tone(8,233,62);//Bb3
+delay(62);
+tone(8,233,62);//Bb3
+delay(62);
+tone(8,588,250);//D5
+delay(250);
+tone(8,440,375);//A4
+delay(375);
+tone(8,415,125);//Ab4
+delay(250);
+tone(8,392,250);//G4
+delay(250);
+tone(8,349,250);//F4
+delay(250);
+tone(8,294,125);//D4
+delay(125);
+tone(8,349,125);//F4
+delay(125);
+tone(8,392,125);//G4
+delay(125);*/
 
   // Passer les lignes D2, D3 et D4 en état bas
   for (unsigned char i = 2; i < 5; i++) {
@@ -20,61 +199,67 @@ void setup() {
     delay(10);
   }
 
-  // Initialiser le premier capteur avec l'adresse par défaut (0x20)
+  // Initialiser le premier capteur en changeant l'adresse (0x20)
   digitalWrite(2, HIGH);
   delay(10);
-  sensor1.init();
-  sensor1.configureDefault();
-  sensor1.setTimeout(500);
-  sensor1.setAddress(0x20);
-  sensor1.startRangeContinuous();
+  capteurG.init();
+  capteurG.configureDefault();
+  capteurG.setTimeout(500);
+  capteurG.setAddress(0x20);
+  capteurG.startRangeContinuous();
 
   // Initialiser le deuxième capteur en changeant l'adresse (0x21)
   digitalWrite(3, HIGH);
   delay(10);
-  sensor2.init();
-  sensor2.configureDefault();
-  sensor2.setTimeout(500);
-  sensor2.setAddress(0x21);
-  sensor2.startRangeContinuous();
+  capteurF.init();
+  capteurF.configureDefault();
+  capteurF.setTimeout(500);
+  capteurF.setAddress(0x21);
+  capteurF.startRangeContinuous();
 
   // Initialiser le troisième capteur en changeant l'adresse (0x22)
   digitalWrite(4, HIGH);
   delay(10);
-  sensor3.init();
-  sensor3.configureDefault();
-  sensor3.setTimeout(500);
-  sensor3.setAddress(0x22);
-  sensor3.startRangeContinuous();
+  capteurD.init();
+  capteurD.configureDefault();
+  capteurD.setTimeout(500);
+  capteurD.setAddress(0x22);
+  capteurD.startRangeContinuous();
 
   // Attribuer les servomoteurs aux broches A1 et A2
-  servo1.attach(A1);
-  servo2.attach(A2);
+  servoG.attach(A1);
+  servoD.attach(A2);
 }
 
 void loop() {
   // Obtenir la valeur des capteurs de distance
-  distance1 = sensor1.readRangeContinuous();
-  distance2 = sensor2.readRangeContinuous();
-  distance3 = sensor3.readRangeContinuous();
+  distanceG = capteurG.readRangeContinuous();
+  distanceF = capteurF.readRangeContinuous();
+  distanceD = capteurD.readRangeContinuous();
 
   // Contrôler les moteurs en fonction des obstacles
-  if (distance1 < 100 && distance2 < 100 && distance3 < 100) {
-    servo1.write(90); // de 91 à 180 -> marche arrière, réciproque vraie ;; 90 = arrêt
-    servo2.write(90);  
+  if (distanceF < 60 && distanceG < 60 && distanceD < 60) {
+    servoG.write(0);
+    servoD.write(135);
+    delay(500);
+  } else if (distanceF < distanceD && distanceG + 25 < distanceD) {
+    servoG.write(180);
+    servoD.write(90);
+  } else if (distanceF < distanceG && distanceD + 25 < distanceG) {
+    servoG.write(90);
+    servoD.write(0);
   } else {
-    servo1.write(0);
-    servo2.write(0);
+    servoG.write(180);
+    servoD.write(0);
   }
 
   // Débuggage capteurs de distance
-  Serial.print("Distance1: ");
-  Serial.print(distance1);
-  Serial.print(" - Distance2: ");
-  Serial.print(distance2);
-  Serial.print(" - Distance3: ");
-  Serial.println(distance3);
-
-  // petit délai
-  delay(50);
+  /*
+  Serial.print("DistanceG: ");
+  Serial.print(distanceG);
+  Serial.print(" - DistanceF: ");
+  Serial.print(distanceF);
+  Serial.print(" - DistanceD: ");
+  Serial.println(distanceD);
+  */
 }
